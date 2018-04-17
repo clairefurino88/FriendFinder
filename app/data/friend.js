9b26e98713a524cdb7cf5a9e9d7aa6c4f1ca757b
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-04-17 14:13:16 -0400

    finished everything

---
+++ app/data/friend.js
@@ -0,0 +1,16 @@
+var friendsArray = [
+    {
+        friendName: "Scarlett Johansson",
+        friendPicture: "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzkxOTI1MjYy/scarlett-johansson-13671719-1-402.jpg",
+        surveyResults: [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ]
+    },
+    {
+    
+        friendName: "Mila Kunis",
+        friendPicture: "http://3m3y892ngk5k1mjv4e2in6jm.wpengine.netdna-cdn.com/wp-content/uploads/2017/05/Mila-Kunis-Desktop.png",
+        surveyResults: [ '7', '7', '7', '7', '7', '7', '7', '7', '7', '7']
+    }
+    ];
+    
+    
+    module.exports = friendsArray;
